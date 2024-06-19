0d651b5bb2082e93c502c19a3af11321a3831cc5
a-drew <a-drew@users.noreply.github.com>
2024-06-19 14:41:38 +0000

    fix styling

--- src/Exceptions/DriverException.php
+++ src/Exceptions/DriverException.php
@@ -2,6 +2,4 @@
 
 namespace Plank\Polyglot\Exceptions;
 
-class DriverException extends \Exception
-{
-}
+class DriverException extends \Exception {}
--- src/Exceptions/ValidationException.php
+++ src/Exceptions/ValidationException.php
@@ -2,6 +2,4 @@
 
 namespace Plank\Polyglot\Exceptions;
 
-class ValidationException extends \InvalidArgumentException
-{
-}
+class ValidationException extends \InvalidArgumentException {}
