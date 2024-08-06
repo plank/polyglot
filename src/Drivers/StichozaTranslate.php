0ef204a65ddc9636abc73996a4d9a8e47abc99bc
Andrew H <andrew@plank.co>
2024-08-06 09:48:38 -0400

    chore: bump stichoza dependency, languages crawl pushed upstream

--- src/Drivers/StichozaTranslate.php
+++ src/Drivers/StichozaTranslate.php
@@ -28,29 +28,6 @@ class StichozaTranslate extends AbstractTranslator
 
     public function languages($target = null): array
     {
-        $client = new Client;
-        $menu = 'sl'; // 'tl';
-        $url = "https://translate.google.com/m?mui=$menu&hl=$target";
-
-        $html = $client->get($url)->getBody()->getContents();
-        // add a meta tag to ensure the HTML content is treated as UTF-8, fixes xpath node values
-        $html = preg_replace('/<head>/i', '<head><meta charset="UTF-8">', $html);
-
-        // Prepare to crawl DOM
-        $dom = new \DOMDocument;
-        $dom->loadHTML($html);
-        $nodes = (new \DOMXPath($dom))->query('//div[@class="language-item"]/a');
-
-        $languages = [];
-        foreach ($nodes as $node) {
-            $code = strtok(str_after($node->getAttribute('href'), "$menu="), '&');
-            if ($target === null) {
-                $languages[] = $code;
-            } else {
-                $languages[$code] = $node->nodeValue;
-            }
-        }
-
-        return $languages;
+        return $this->client->languages($target);
     }
 }
